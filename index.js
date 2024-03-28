66dde4b40949c6bf2f77b02ca577b131d6695088
VinoliaTsitsi <tsitsivinolia07@gmail.com>
2024-03-28 14:14:19 +0200

    I am working on the display function

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Sample menu data (Consider fetching this data from a server in a real-world scenario)
-const menu = {
+let menu ={
     Starters: ["Garlic Bread", "Bruschetta"],
     MainCourses: ["Margherita Pizza", "Spaghetti Carbonara"],
     Desserts: ["Tiramisu", "Cheesecake"]
@@ -8,9 +8,25 @@ const menu = {
 // Function to display menu items by category
 function displayMenuItems(menu) {
     // Get the menu container element from the HTML
-
+const menueContainer = document.getElementById('menu');
     // Loop through each category and its items in the menu object
+for (const category in menu){
 
+    if (menu.hasOwnProperty(category)){
+
+    const categoryElement = document.createElement('div');
+    categoryElement.innerHTML = `<h3>${category}<h3/>`;
+    menueContainer.appendChild(categoryElement);  
+    }
+}
+const itemList = document.createElement('ul'); 
+menueContainer.appendChild(itemList); 
+
+menu[category].forEach(item => {
+    const listItem = document.createElement('li'); 
+    listItem.textContent = item;
+    listItem.addEventListener('click', () => addToorder)
+    }
         // Create an element to represent the category
 
         // Set the text content of the category element to the category name
